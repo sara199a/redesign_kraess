66f0140c38207530af85c76c395bf13fc62502eb
Sarah Hvid <sara199a@stud.kea.dk>
2022-06-02 16:01:32 +0200

    ændret java

--- java.js
+++ java.js
@@ -1,25 +1,32 @@
-// Lav variablen "btn", der henviser til ".toggle-btn"
-// Lav variablen "menu", der henviser til ".main-menu"
-const btn = document.querySelector(".toggle-btn");
-const menu = document.querySelector(".menu");
+// // Lav variablen "btn", der henviser til ".toggle-btn"
+// // Lav variablen "menu", der henviser til ".main-menu"
+// const btn = document.querySelector(".toggle-btn");
+// const menu = document.querySelector(".menu");
 
-// Lav funktionen "toggleMenu()" med følgende funktionalitet
-function toggleMenu() {
-  // a) toggle klassen "shown" på menu vha. classList.toggle
-  menu.classList.toggle("shown");
-  let menuShown = menu.classList.contains("shown");
-  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
+// // Lav funktionen "toggleMenu()" med følgende funktionalitet
+// function toggleMenu() {
+//   // a) toggle klassen "shown" på menu vha. classList.toggle
+//   menu.classList.toggle("shown");
+//   let menuShown = menu.classList.contains("shown");
+//   // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
 
-  // // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
-  // if (menuShown) {
-  //   console.log(menuShown); // se i konsollen
-  //   // sæt btn.textContent til "Luk", hvis menuShown er "true"
-  //   btn.textContent = "Luk";
-  // } else {
-  //   console.log(menuShown); // se i konsollen
-}
-// "toggleMenu()" slutter her
+//   // // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+//   // if (menuShown) {
+//   //   console.log(menuShown); // se i konsollen
+//   //   // sæt btn.textContent til "Luk", hvis menuShown er "true"
+//   //   btn.textContent = "Luk";
+//   // } else {
+//   //   console.log(menuShown); // se i konsollen
 // }
+// // "toggleMenu()" slutter her
+// // }
 
-// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
-btn.addEventListener("click", toggleMenu);
+// // Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+// btn.addEventListener("click", toggleMenu);
+
+const toggleButton = document.getElementById("toggle-button");
+const navList = document.getElementById("navi-list");
+
+toggleButton.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
